8066c09d8ead502f060b60fee801320b91bd0aaf
srigorti <34012006+srigorti@users.noreply.github.com>
2020-02-06 22:34:10 -0600

    Update FetchWorks_PyramidWord.py
    Validation fixes

--- FetchWorks_PyramidWord.py
+++ FetchWorks_PyramidWord.py
@@ -15,15 +15,15 @@ def pyramidWord(word):
 
     if len(countList) == 1 and countList[0] == 1:
         return json.dumps(True)
-    else:
+    elif len(countList) == 1:
         return json.dumps(False)
-
-    # operations on sets are efficient than on Lists
-    countSet = set(countList)
-
-    # If there are no duplicate counts occuring
-    if len(countSet) == len(countList):
-        # check if consecutive numbers are present in the list
-        return json.dumps(sorted(countSet) == list(range(min(countSet), max(countSet)+1)))
     else:
-        return json.dumps(False)
+        # operations on sets are efficient than on Lists
+        countSet = set(countList)
+
+        # If there are no duplicate counts occuring
+        if len(countSet) == len(countList):
+            # check if consecutive numbers are present in the list
+            return json.dumps(sorted(countSet) == list(range(min(countSet), max(countSet)+1)))
+        else:
+            return json.dumps(False)
